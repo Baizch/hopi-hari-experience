17f84b168e8042ca6e2d23331720c9f7c3342c67
baizch <francobaisch00@gmail.com>
2024-09-09 15:08:39 -0300

    footer

--- src/App.vue
+++ src/App.vue
@@ -1,9 +1,12 @@
 <script setup lang="ts">
 import Header from './components/header/Header.vue';
+import Footer from './components/footer/Footer.vue';
 </script>
 
 <template>
   <Header />
+  <Footer />
+
 </template>
 
 <style scoped></style>
--- src/components/header/Header.vue
+++ src/components/header/Header.vue
@@ -1,6 +1,6 @@
 <template>
     <div>
-        <nav className="sticky top-0 py-6 bg-sky-700 text-white font-bold flex items-center justify-center shadow">
+        <nav className="sticky top-0 py-6 bg-neutral-600 text-white font-bold flex items-center justify-center shadow">
             <p>Hopi Hari Experience</p>
         </nav>
     </div>
