8f11bbc28fc804c94c4afab15c4d9c69056fe2ee
baizch <francobaisch00@gmail.com>
2024-09-12 14:38:03 -0300

    card component

--- src/sections/attractions/children/Children.vue
+++ src/sections/attractions/children/Children.vue
@@ -1,22 +1,12 @@
 <script setup lang="ts">
 import { ref } from 'vue';
+import { ChildrenAttractions } from '../../../types';
 
 import Subtitle from '../../../components/subtitle/Subtitle.vue';
 import Modal from '../../../components/modal/Modal.vue';
+import Card from '../../../components/card/Card.vue';
 
 import attractions from '../attractions.json';
-
-interface ChildrenAttractions {
-  name: string;
-  description: string;
-  status: string;
-  openingHours: string;
-  minHeight: string;
-  maxHeight: string;
-  image: string;
-  altImage: string;
-  video: string;
-}
 
 const list: ChildrenAttractions[] = attractions.attractions.children;
 
@@ -39,45 +29,5 @@ const subtitle: string = 'Infantis';
 <template>
   <Modal :open="open" :closeModal="closeModal" :selectedItem="selectedItem" />
   <Subtitle :subtitle="subtitle" />
-
-  <!-- TO DO: componetizar -->
-  <div class="grid lg:grid-cols-4 md:grid-cols-3 gap-4 mt-14 mb-64">
-    <div
-      class="rounded overflow-hidden shadow-lg mb-2"
-      v-for="(item, index) in list"
-      :key="index"
-    >
-      <img class="w-full" :src="item.image" :alt="item.altImage" />
-      <div class="px-6 py-4">
-        <div class="font-bold text-xl mb-2">{{ item.name }}</div>
-        <p class="text-gray-700 text-base">
-          {{ item.description }}
-        </p>
-        <p class="text-gray-700 text-base mt-2 space-x-1">
-          <strong>Status:</strong>
-          <span
-            class="inline-flex items-center rounded-md bg-green-50 px-2 py-1 text-xs font-medium text-green-700 ring-1 ring-inset ring-green-600/20"
-            >{{ item.status }}</span
-          >
-        </p>
-        <p class="text-gray-700 text-base mt-2">
-          <strong>Horário de funcionamento:</strong> {{ item.openingHours }}
-        </p>
-        <div class="flex space-x-20">
-          <p class="text-gray-700 text-base mt-2">
-            <strong>Altura mínima: </strong> {{ item.minHeight }}
-          </p>
-          <p class="text-gray-700 text-base mt-2">
-            <strong>Altura máxima: </strong> {{ item.maxHeight }}
-          </p>
-        </div>
-        <button
-          class="mt-4 font-extrabold bg-white hover:bg-gray-100 text-gray-800 py-2 px-4 border border-gray-400 rounded shadow"
-          @click="showModal(item)"
-        >
-          Ver vídeo
-        </button>
-      </div>
-    </div>
-  </div>
+  <Card :list="list" :showModal="showModal" />
 </template>
