8e26943f5e94f29d893ef85a5774c5df1d660e22
baizch <francobaisch00@gmail.com>
2024-09-12 12:17:52 -0300

    modal component

---
+++ src/components/modal/Modal.vue
@@ -0,0 +1,41 @@
+<script setup lang="ts">
+import { PropType } from 'vue';
+import { ChildrenAttractions } from '../../types';
+
+const props = defineProps({
+  open: {
+    type: Boolean,
+    required: true,
+  },
+  closeModal: {
+    type: Function,
+    required: true,
+  },
+  selectedItem: {
+    type: Object as PropType<ChildrenAttractions>,
+    required: true,
+  },
+});
+</script>
+
+<template>
+  <a-modal
+    v-if="props.selectedItem"
+    v-model:open="props.open"
+    :title="props.selectedItem.name"
+    :closable="false"
+    :destroyOnClose="true"
+  >
+    <video class="h-full w-full rounded-lg mt-4" controls>
+      <source :src="props.selectedItem.video" type="video/mp4" />
+    </video>
+    <template #footer>
+      <button
+        @click="props.closeModal"
+        class="mt-4 font-extrabold bg-white hover:bg-gray-100 text-gray-800 py-2 px-4 border border-gray-400 rounded shadow"
+      >
+        Fechar
+      </button>
+    </template>
+  </a-modal>
+</template>
---
+++ src/types/index.ts
@@ -0,0 +1,11 @@
+export interface ChildrenAttractions {
+  name: string;
+  description: string;
+  status: string;
+  openingHours: string;
+  minHeight: string;
+  maxHeight: string;
+  image: string;
+  altImage: string;
+  video: string;
+}
