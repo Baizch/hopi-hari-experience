e11009c27224f0deb2a2b3f47caf26235a9119ad
baizch <francobaisch00@gmail.com>
2024-09-12 12:30:51 -0300

    title component

---
+++ src/components/title/Title.vue
@@ -0,0 +1,7 @@
+<script setup lang="ts">
+import title from './title.json';
+</script>
+
+<template>
+  <h2 class="font-bold text-3xl uppercase">{{ title.text }}</h2>
+</template>
--- src/sections/attractions/Attractions.vue
+++ src/sections/attractions/Attractions.vue
@@ -1,12 +1,12 @@
 <script setup lang="ts">
+import Title from '../../components/title/Title.vue';
 import Children from './children/Children.vue';
 </script>
 
 <template>
   <section>
     <div class="mt-12 max-w-full mx-auto px-6 text-center">
-      <h2 class="font-bold text-3xl uppercase">Atrações</h2>
-
+      <Title />
       <Children />
     </div>
   </section>
